0799ea872a8f0a361592ed69eb1fefe63306c7e4
Paul Martin <Dmart331@gmail.com>
2016-10-18 23:15:38 -0500

    Added for loop for removing unwantd

---
+++ songs.js
@@ -0,0 +1,16 @@
+var songs = [];
+
+songs[songs.length] = "Legs - by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song - by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall - by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle - by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c - by Alanis Moris*ette on the album Jagged Little Pill";
+
+for(i=0; i<songs.length; i++){
+	songs[i] = songs[i].replace('*', "");
+	songs[i] = songs[i].replace('@' , "");
+	songs[i] = songs[i].replace('(' , "");
+	songs[i] = songs[i].replace('!' , "");
+}
+console.log(songs);
+
